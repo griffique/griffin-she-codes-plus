66a48fbde152348f2f8862b63b81f1fee5c1113d
Quinn <74791898+griffique@users.noreply.github.com>
2021-01-10 17:59:31 -0500

    replaced separate forecast functions with a loop

--- src/index.js
+++ src/index.js
@@ -45,56 +45,18 @@ function search(city) {
 }
 function displayForecast(response) {
   let forecast = null;
-  forecast = response.data.list[0];
   let forecastDisplay = document.querySelector("#hourly-forecast-display");
-
-  forecastDisplay.innerHTML = ` <div class="card col-sm-2"><h5>${formatDate(
-    forecast.dt * 1000
-  )}</h5> <img class= "card-img" src="https://openweathermap.org/img/wn/${
-    forecast.weather[0].icon
-  }@2x.png" alt=${forecast.description}/><h6>${Math.round(
-    forecast.main.temp
-  )}°</h6></div>`;
-  forecast = response.data.list[1];
-  forecastDisplay.innerHTML += ` <div class="card col-sm-2"><h5>${formatDate(
-    forecast.dt * 1000
-  )}</h5> <img class= "card-img" src="https://openweathermap.org/img/wn/${
-    forecast.weather[0].icon
-  }@2x.png" alt=${forecast.description}/><h6>${Math.round(
-    forecast.main.temp
-  )}°</h6></div>`;
-  forecast = response.data.list[2];
-  forecastDisplay.innerHTML += ` <div class="card col-sm-2"><h5>${formatDate(
-    forecast.dt * 1000
-  )}</h5> <img class= "card-img"  src="https://openweathermap.org/img/wn/${
-    forecast.weather[0].icon
-  }@2x.png" alt=${forecast.description}/><h6>${Math.round(
-    forecast.main.temp
-  )}°</h6></div>`;
-  forecast = response.data.list[3];
-  forecastDisplay.innerHTML += ` <div class="card col-sm-2"><h5>${formatDate(
-    forecast.dt * 1000
-  )}</h5> <img class= "card-img" src="https://openweathermap.org/img/wn/${
-    forecast.weather[0].icon
-  }@2x.png" alt=${forecast.description}/><h6>${Math.round(
-    forecast.main.temp
-  )}°</h6></div>`;
-  forecast = response.data.list[4];
-  forecastDisplay.innerHTML += ` <div class="card col-sm-2"><h5>${formatDate(
-    forecast.dt * 1000
-  )}</h5> <img class= "card-img" src="https://openweathermap.org/img/wn/${
-    forecast.weather[0].icon
-  }@2x.png" alt=${forecast.description}/><h6>${Math.round(
-    forecast.main.temp
-  )}°</h6></div>`;
-  forecast = response.data.list[5];
-  forecastDisplay.innerHTML += ` <div class="card col-sm-2"><h5>${formatDate(
-    forecast.dt * 1000
-  )}</h5> <img class= "card-img"  src="https://openweathermap.org/img/wn/${
-    forecast.weather[0].icon
-  }@2x.png" alt=${forecast.description}/><h6>${Math.round(
-    forecast.main.temp
-  )}°</h6></div>`;
+  forecastDisplay.innerHTML = null;
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastDisplay.innerHTML += ` <div class="card col-sm-2"><h5>${formatDate(
+      forecast.dt * 1000
+    )}</h5> <img class= "card-img" src="https://openweathermap.org/img/wn/${
+      forecast.weather[0].icon
+    }@2x.png" alt=${forecast.description}/><h6>${Math.round(
+      forecast.main.temp
+    )}°</h6></div>`;
+  }
 }
 
 function handleSubmit(event) {
