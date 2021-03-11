b1fc04f40c4ed35865b3149c737e38516871bc7a
Quinn <74791898+griffique@users.noreply.github.com>
2021-03-11 11:49:09 -0500

    edited text of last update time

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ function showTemp(response) {
   let currentTime = new Date(response.data.dt * 1000);
   let currentTimeDisplay = document.querySelector("#current-time");
   let formattedTime = formatDate(currentTime);
-  currentTimeDisplay.innerHTML = `Last updated ${formattedTime}`;
+  currentTimeDisplay.innerHTML = `Last updated at ${formattedTime}`;
   let iconDisplay = document.querySelector("#icon-element");
   iconDisplay.setAttribute(
     "src",
