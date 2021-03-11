75ef8ea95faca8e9aad3e97227e08cc3ef505872
Quinn <74791898+griffique@users.noreply.github.com>
2021-03-11 11:48:14 -0500

    Removed morning mode colors

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ function change_background() {
   let day = new Date();
   let now = day.getHours();
 
-  if (now > 5 && now < 12) {
-    document.body.className = "morning";
-  } else if (now > 12 && now < 18) {
+  if (now > 5 && now < 18) {
     document.body.className = "day";
   } else {
     document.body.className = "night";
